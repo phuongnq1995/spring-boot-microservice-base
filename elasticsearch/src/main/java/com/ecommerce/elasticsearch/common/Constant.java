cfa3d67d965bf06c0370e3c0ab8a04221276ff2d
dungnq94it <dungnq.itedu@gmail.com>
2020-08-10 21:53:28 +0700

    Remove province

--- elasticsearch/src/main/java/com/ecommerce/elasticsearch/common/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.elasticsearch.common;
-
-public class Constant {
-
-    public static final String EMPTY_DEFAULT_VALUE = "";
-    public static final Integer LIMIT_DEFAULT_VALUE = 20;
-    public static final Integer OFFSET_DEFAULT_VALUE = 1;
-    
-}
--- elasticsearch/src/main/java/com/ecommerce/elasticsearch/component/ElacticeService.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.elasticsearch.component;
-
-public interface ElacticeService<E, R> {
-
-    ResultSetData<E> getListSource(R source);
-
-    ResultData<E> createSource(E source);
-}
